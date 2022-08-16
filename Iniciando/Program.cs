77f85f804719da59c39443370fb850e90d4e4ef6
nancymorales1994 <nancy.morales.387.06@gmail.com>
2022-08-16 11:59:42 -0300

    Agregar archivos de proyecto.

---
+++ Iniciando/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace aperturaDeArchivoConExcepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.IO.StreamReader archivo = null;
+
+            try
+            {
+                string linea;
+                int contador = 0;
+                string path = @"C:\Users\Carolina\Desktop\texto.txt";
+                archivo = new System.IO.StreamReader(path);
+
+                while ((linea=archivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                    contador++;
+                    
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+            finally
+            {
+                if (archivo != null)
+                {
+                    archivo.Close();
+                    Console.WriteLine("Conexion con el fichero cerrada");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Programa terminado");
+            Console.ReadLine();
+        }
+
+       
+    } 
+}
